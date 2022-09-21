2a673c0af2f5c5225fab437e51a4b7d276c24d43
Thibault Thuillier <thibault.thuillier@epitech.eu>
2022-09-21 17:21:13 +0200

    feat: content class

---
+++ src/models/content.ts
@@ -0,0 +1,21 @@
+export default class Content {
+    id: number;
+    title: string;
+    picture: string;
+    mark: number;
+    published: number;
+    
+    constructor(
+     id: number,
+     title: string = 'title',
+     picture: string = 'https://...',
+     mark: number = 5,
+     published: number = 2022
+    ) { 
+     this.id = id;
+     this.title = title;
+     this.picture = picture;
+     this.mark = mark;
+     this.published = published;
+    }
+}
