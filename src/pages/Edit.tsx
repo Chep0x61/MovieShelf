8599dbfa64bd8500d1a32f788ea6d1626c864a9e
Thibault Thuillier <thibault.thuillier@epitech.eu>
2022-09-23 00:51:27 +0200

    fix: typo

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -6,27 +6,26 @@ import ContentForm from '../components/contentForm';
 import { cp } from 'fs';
 
 const ContentEdit: FunctionComponent = () => {
-    const navigate = useNavigate();
-    const {id} = useParams();
-    const [content, setContent] = useState<Content|null>(null);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const [content, setContent] = useState<Content|null>(null);
 
+  useEffect(() => {
+		ContentService.getContent(+id!).then(content => setContent(content))
+	}, [id]);
 
-    useEffect(() => {
-        ContentService.getContent(+id!).then(content => setContent(content))
-    }, [id]);
-    
-    return (
-        <div>
-            { content ? (
-                <div className='row'>
-                    <h2 className="header center">Edit {content.title}</h2>
-                    <ContentForm content={content} isEditForm={true}></ContentForm>
-                </div>
-            ) : (
-                <h4 className='center'>No Content to display</h4>
-            )}
+  return (
+    <div>
+			{ content ? (
+      	<div className='row'>
+          	<h2 className="header center">Edit {content.title}</h2>
+              <ContentForm content={content} isEditForm={true}></ContentForm>
         </div>
-    );
+      )	:	(
+        <h4 className='center'>No Content to display</h4>
+      )}
+    </div>
+  );
 }
 
 export default ContentEdit;
